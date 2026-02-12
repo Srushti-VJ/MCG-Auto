52134cac63c8ba7b3208ec9c414d796e17df59b6
Srushti <srushtij16@gmail.com>
2026-02-12 13:20:34 +0530

    footer section

---
+++ Data/footer.data.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mcibpl: {
+    textValidations: [
+      'Marwadi Chandarana Intermediaries Brokers Private Limited',
+      'CIN: U67120GJ2018PTC103598',
+      'Registered Office:',
+      'Corporate Office:',
+      'SEBI Reg. No. for Stock Broker: INZ000250531',
+      'SEBI Reg. No. for Merchant Banking',
+      'Investment Advisor: INA000021517'
+    ]
+  
+   },
+
+  msfl: {
+    key: 'MSFL',
+    textValidations: [
+      'Marwadi Shares and Finance Limited',
+      'CIN: U65910GJ1992PLC017544',
+      'Registered Office:',
+      'Corporate Office:',
+      'SEBI Reg. No. of for Stock Broker: INZ000174730'
+    ]
+  }
+
+};
